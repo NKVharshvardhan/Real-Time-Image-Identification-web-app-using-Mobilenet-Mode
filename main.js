47a4f342a1e47eb12252ebc409c34bb31fa3e8db
NKVharshvardhan <73694587+NKVharshvardhan@users.noreply.github.com>
2021-07-02 14:34:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,26 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier=ml5.imageClassifier('MobileNet',modelloaded);
 }
 
+function modelloaded(){
+  console.log("modelloaded");
+}
 
+function draw(){
+  image(video,0,0,300,300);
+classifier.classify(video,gotResult);
+}
 
+function gotResult(error,results){
+if(error){
+console.error(error);
+}
+else{
+  console.log(results);
+  document.getElementById("result-object-name").innerHTML=results[0].label;
+  document.getElementById("result-object-accuracy").innerHTML=results[0].confidence.toFixed(3);
+
+}
+}
